e976972a42b6508b51e225c2e4b0d5a66e20853f
Comandeer <vepomoc@gmail.com>
2016-04-19 21:06:53 +0200

    Fixed issues with 404 error page.

--- grunt/htmlmin.js
+++ grunt/htmlmin.js
@@ -1,15 +1,16 @@
 module.exports = {
 	dist: {
 		options: {
-			removeComments: true
-			,collapseWhitespace: true
-		}
-		,files: {
-			'dist/index.html': 'dist/index.html'
-			,'dist/o-mnie.html': 'dist/o-mnie.html'
-			,'dist/projekty.html': 'dist/projekty.html'
-			,'dist/kontakt.html': 'dist/kontakt.html'
-			,'dist/cookies.html': 'dist/cookies.html'
+			removeComments: true,
+			collapseWhitespace: true
+		},
+		files: {
+			'dist/index.html': 'dist/index.html',
+			'dist/o-mnie.html': 'dist/o-mnie.html',
+			'dist/projekty.html': 'dist/projekty.html',
+			'dist/kontakt.html': 'dist/kontakt.html',
+			'dist/cookies.html': 'dist/cookies.html',
+			'dist/404.html': 'dist/404.html'
 		}
 	}
 };
