eb421ead1cb7e874263e4092f7f2cf4356c282f9
Tomasz Jakut <vepomoc@gmail.com>
2016-12-21 22:28:32 +0100

    style(zoomer): get rid of function expressions

--- js/zoom.js
+++ js/zoom.js
@@ -4,44 +4,47 @@
 		var overlay = document.createElement( 'div' ),
 			notification = document.createElement( 'span' ),
 			img = document.createElement( 'img' ),
-			open = function( src, alt, element ) {
-				img.src = src;
-				img.alt = alt;
-
-				overlay.setAttribute( 'aria-hidden', false );
-				overlay.classList.add( 'zoomer_visible' );
-
-				notification.innerHTML = 'Otworzono powiększenie screenshota. Naciśnij <kbd>Esc</kbd>, aby zamknąć.';
-
-				previouslyFocused = element;
-
-				document.addEventListener( 'click', close, false );
-				document.addEventListener( 'keydown', escHandler, false );
-			},
-			escHandler = function( evt ) {
-				if ( evt.keyCode !== 27 ) {
-					return true;
-				}
-
-				evt.preventDefault();
-				close();
-			},
-			close = function() {
-				img.src = 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7';
-				img.alt = '';
-
-				overlay.setAttribute( 'aria-hidden', true );
-				overlay.classList.remove( 'zoomer_visible' );
-
-				notification.innerHTML = '';
-
-				previouslyFocused.focus();
-				previouslyFocused = null;
-
-				document.removeEventListener( 'click', close, false );
-				document.removeEventListener( 'keydown', escHandler, false );
-			},
 			previouslyFocused;
+
+		function open( src, alt, element ) {
+			img.src = src;
+			img.alt = alt;
+
+			overlay.setAttribute( 'aria-hidden', false );
+			overlay.classList.add( 'zoomer_visible' );
+
+			notification.innerHTML = 'Otworzono powiększenie screenshota. Naciśnij <kbd>Esc</kbd>, aby zamknąć.';
+
+			previouslyFocused = element;
+
+			document.addEventListener( 'click', close, false );
+			document.addEventListener( 'keydown', escHandler, false );
+		}
+
+		function escHandler( evt ) {
+			if ( evt.keyCode !== 27 ) {
+				return true;
+			}
+
+			evt.preventDefault();
+			close();
+		}
+
+		function close() {
+			img.src = 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7';
+			img.alt = '';
+
+			overlay.setAttribute( 'aria-hidden', true );
+			overlay.classList.remove( 'zoomer_visible' );
+
+			notification.innerHTML = '';
+
+			previouslyFocused.focus();
+			previouslyFocused = null;
+
+			document.removeEventListener( 'click', close, false );
+			document.removeEventListener( 'keydown', escHandler, false );
+		};
 
 		img.classList.add( 'zoomer__image' );
 		img.alt = '';
@@ -54,7 +57,7 @@
 		overlay.appendChild( notification );
 		document.body.appendChild( overlay );
 
-		for( var zoomLinks = document.getElementsByClassName( 'screenshot__zoom' ), i = 0; i < zoomLinks.length; ++i )
+		for ( var zoomLinks = document.getElementsByClassName( 'screenshot__zoom' ), i = 0; i < zoomLinks.length; ++i ) {
 			zoomLinks[ i ].addEventListener( 'click', function( evt ) {
 				if ( !this.href ) {
 					return true;
@@ -65,5 +68,6 @@
 
 				open( this.href, this.parentNode.parentNode.getElementsByClassName( 'project__name' )[ 0 ].textContent + ' – screenshoot', this );
 			}, false );
+		}
 	}
 } () );
